f6cd6afff01c6dcbbe82d7981ce0c649b2a35189
Salil Thakur <156768025+salilthakur9@users.noreply.github.com>
2024-10-09 03:19:05 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,7 +19,7 @@ const Header = () => {
           </p>
         </div>
         <a href='' className='text-white text-base md:text-lg flex items-center'>
-          Book Appointment  messi better
+          Book Appointment
           <img src={assets.arrow_icon} alt='arrow' className='ml-2' />
         </a>
       </div>
