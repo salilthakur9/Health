5b25b62a50e437d48cb88b2aecdede20e421b720
Salil Thakur <salilthakur09@gmail.com>
2024-10-12 03:35:30 +0530

    upto banner.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 export const App = () => {
   return (
@@ -25,6 +26,7 @@ export const App = () => {
         <Route path='/my-appointments' element={<MyAppointments />}/>
         <Route path='/appointments/:docId' element={<Appointment />}/>
       </Routes>
+      <Footer />
       </div>
   )
 }
