d5fa8bf8d47e1c4fadcd6784890104b139734115
Salil Thakur <salilthakur09@gmail.com>
2024-10-17 01:27:28 +0530

    upto Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+    <div className='text-center text-2xl pt-10 text-green-500'>
+      <p>CONTACT <span className='text-green-800 font-semibold'>US</span></p>
+    </div>
+
+    <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+      <img className='w-full md:max-w-[360px]' src={assets.contact_image} />
+      <div className='flex flex-col justify-center items-start gap-6'>
+        <p className='font-semibold text-lg text-green-600'>OUR OFFICE</p>
+        <p className='text-green-500'>Chitkara University <br />Rajpura, Punjab, INDIA</p>
+        <p className='text-green-500'>Tel: +91 1234567890 <br />Email: salil@gmail.com</p>
+        <p className='font-semibold text-lg text-green-600'>Careers at Health-Sphere</p>
+        <p className='text-green-500'>Learn more about our team and job openings</p>
+        <button className='border border-green-800 px-8 py-4 text-green-800 text-sm hover:bg-green-800 hover:text-white transition-all duration-300'>Explore Jobs</button>
+      </div>
+    </div>
+    </div>
   )
 }
 
