8dbde267366a73d2433943b8224c59792f3d7980
Salil Thakur <salilthakur09@gmail.com>
2024-10-28 18:56:03 +0530

    generated user login API

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,30 @@
-import express from 'express'
-import cors from 'cors'                               
-import 'dotenv/config'                     // get the support of environment variables
-import connetDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
+import doctorRouter from './routes/doctorRoute.js';
+import userRouter from './routes/userRoute.js';
 
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
 
-// app config
-const app = express()            
-const port = process.env.PORT || 4000
-connetDB()
-connectCloudinary()
+connectDB();
+connectCloudinary();
 
-// middlewares
-app.use(express.json())         // whenever we made an req it will get passs using this 
-app.use(cors())                   // it will allow frontend to connect with backend
+// Middlewares
+app.use(express.json());
+app.use(cors());
 
+// API endpoints
+app.use('/api/admin', adminRouter);
+app.use('/api/doctor', doctorRouter);
+app.use('/api/user', userRouter);
 
-//API endpoints
-app.use('/api/admin', adminRouter)
-app.use('/api/doctor', doctorRouter)
+app.get('/', (req, res) => {
+    res.send('API WORKING !');
+});
 
-app.get('/',(req, res)=>{
-    res.send('API WORKING !')
-})
-
-app.listen(port, ()=>console.log("Server Started", port))
+app.listen(port, () => console.log("Server Started on port", port));
