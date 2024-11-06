742ad85f288f71f9642ec508e006b322ec53b06d
Salil Thakur <salilthakur09@gmail.com>
2024-11-06 18:26:30 +0530

    updated for an api of payment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken';
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js';
 import appointmentModel from '../models/appointmentModel.js';
+import razorpay from 'razorpay';
 
 // API to register user:
 const registerUser = async (req, res) => {
@@ -216,5 +217,11 @@ const cancelAppointment = async(req, res)=>{
         res.json({success:false, message: error.message})
     }
 }
+
+
+
+// api to make payment of appointment using razorpay:     /*-------------- 11:40 ----------------*/
+
+
 
 export { registerUser, loginUser, getProfile, updateProfile, bookAppointment, listAppointment, cancelAppointment};
